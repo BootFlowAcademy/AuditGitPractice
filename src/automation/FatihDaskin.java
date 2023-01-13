09db942b27d0e5632f68594b8f30193fb510c4c6
Fatih Daskin <fatih.daskin40@gmail.com>
2023-01-13 23:13:21 +0300

    Fatih Daskin test 2

--- src/automation/FatihDaskin.java
+++ src/automation/FatihDaskin.java
@@ -3,5 +3,6 @@ package automation;
 public class FatihDaskin {
     public static void main(String[] args) {
         System.out.println("Fatih Daskin test");
+        System.out.println("test");
     }
 }
