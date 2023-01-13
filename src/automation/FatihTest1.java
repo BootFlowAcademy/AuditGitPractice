fbd988861f6ebc749943ae8cadfe10793b077a69
Fatih <yolcu.m.fatih@gmail.com>
2023-01-13 22:24:40 +0300

    M.Fatih test added.

--- src/automation/FatihTest1.java
+++ src/automation/FatihTest1.java
@@ -3,5 +3,6 @@ package automation;
 public class FatihTest1 {
     public static void main(String[] args) {
         System.out.println("Fatih Test1 ");
+        System.out.println("-----------------");
     }
 }
