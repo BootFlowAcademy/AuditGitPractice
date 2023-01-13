ef60e1539a058cdd6b76926009a813421fcffacd
SerdarSaygin <serdarsaygin4312@outlook.com>
2023-01-13 22:36:33 +0300

    SerdarTest added

--- src/dataFiles/WedonTest.java
+++ src/dataFiles/WedonTest.java
@@ -1,7 +1,7 @@
 package dataFiles;
 
-public class aaaaWedonTest {
+public class WedonTest {
     public static void main(String[] args) {
-        System.out.println("aaaaa Test");
+        System.out.println("Wedon Test");
     }
 }
