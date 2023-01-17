67485c0b4b3ee3abb8bd4f87df0b5c88e1c58855
ErginDogru <byedogru@gmail.com>
2023-01-17 22:34:55 +0300

    commit ergin test added

--- src/dataFiles/Ergin.java
+++ src/dataFiles/Ergin.java
@@ -3,5 +3,6 @@ package dataFiles;
 public class Ergin {
     public static void main(String[] args) {
         System.out.println("ergin test");
+        System.out.println("token için değişiklik");
     }
 }
